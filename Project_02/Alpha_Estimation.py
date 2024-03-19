4705cce0dd0c2a5d03ab9e47eda212b204ded728
UIC-Hanson <rhans7@uic.edu>
2024-03-18 21:41:20 -0500

    missing parenthesis

--- Project_02/Alpha_Estimation.py
+++ Project_02/Alpha_Estimation.py
@@ -87,5 +87,4 @@ if __name__ == '__main__':
         print("stop and exit")
         sleep(0.1)
         duration = end_time - tracking_start_time
-        print(f"Time taken to run the course: {duration:.2f} seconds"
-        
+        print(f"Time taken to run the course: {duration:.2f} seconds")
