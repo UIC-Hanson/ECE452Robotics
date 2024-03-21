e50c8f5a71020f66db80b7a514f8b2d6defd3357
UIC-Hanson <rhans7@uic.edu>
2024-03-21 10:56:13 -0500

    More remove buzzer

--- Project_02/API_checks.py
+++ Project_02/API_checks.py
@@ -7,7 +7,7 @@ motors = Motors()
 servo = Servo("P0") # Assume a servo is connected to PWM pin 0
 adc = ADC(0) # Assume an ADC device is connected to channel 0
 rgb_led = RGB_LED(PWM("P0"), PWM("P1"), PWM("P2")) # Assuming RGB LED is connected to PWM pins 0, 1, and 2
-buzzer = Buzzer(PWM(0)) # Assuming a passive buzzer is connected to PWM pin 0
+#buzzer = Buzzer(PWM(0)) # Assuming a passive buzzer is connected to PWM pin 0
 ultrasonic = Ultrasonic("D2", "D3") # Assuming ultrasonic sensor's TRIG and ECHO pins are connected to D2 and D3 respectively
 accelerometer = ADXL345() # Initializing ADXL345 accelerometer
 
