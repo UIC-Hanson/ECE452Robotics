0cead420a13d45548541a51920ef9095be46a662
UIC-Hanson <rhans7@uic.edu>
2024-03-21 10:57:16 -0500

    ultrasonic removed

--- Project_02/API_checks.py
+++ Project_02/API_checks.py
@@ -8,7 +8,7 @@ servo = Servo("P0") # Assume a servo is connected to PWM pin 0
 adc = ADC(0) # Assume an ADC device is connected to channel 0
 rgb_led = RGB_LED(PWM("P0"), PWM("P1"), PWM("P2")) # Assuming RGB LED is connected to PWM pins 0, 1, and 2
 #buzzer = Buzzer(PWM(0)) # Assuming a passive buzzer is connected to PWM pin 0
-ultrasonic = Ultrasonic("D2", "D3") # Assuming ultrasonic sensor's TRIG and ECHO pins are connected to D2 and D3 respectively
+#ultrasonic = Ultrasonic("D2", "D3") # Assuming ultrasonic sensor's TRIG and ECHO pins are connected to D2 and D3 respectively
 accelerometer = ADXL345() # Initializing ADXL345 accelerometer
 
 def check_battery_voltage():
@@ -44,8 +44,8 @@ def sensor_readings():
         print(f"ADC Value: {adc_value}")
 
         # Ultrasonic distance
-        distance = ultrasonic.read()
-        print(f"Distance: {distance} cm")
+        #distance = ultrasonic.read()
+        #print(f"Distance: {distance} cm")
 
         # Accelerometer readings
         x, y, z = accelerometer.read()
