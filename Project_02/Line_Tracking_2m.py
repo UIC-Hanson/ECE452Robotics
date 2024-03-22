8328aca770a229be2fc2119e77ae6fc01aeab4a7
UIC-Hanson <rhans7@uic.edu>
2024-03-22 15:52:55 -0500

    modifiy while loop with run statement.

--- Project_02/Line_Tracking_2m.py
+++ Project_02/Line_Tracking_2m.py
@@ -3,8 +3,6 @@ from time import sleep, time
 
 px = Picarx()
 
-last_state = None
-
 def initialize_robot():
     # Initialize settings
     #px.set_grayscale_reference([1400, 1400, 1400])
@@ -12,33 +10,23 @@ def initialize_robot():
     px.set_cam_pan_angle(0)
     px.set_cam_tilt_angle(0)
 
-def handle_out():
-    global last_state
-    if last_state in ['left', 'right']:
-        px.set_dir_servo_angle(-12 if last_state == 'left' else 12)
-        px.backward(5)
-        while px.get_line_status(px.get_grayscale_data()) == last_state:
-            sleep(0.001)
-
 def get_status():
     """Determine the robot's state based on grayscale sensor data."""
-    global last_state
-    val_list = px.get_grayscale_data()
+    val_list=px.get_grayscale_data()
     state = px.get_line_status(val_list)
     
     if state == [0, 0, 0]:
-        last_state = 'stop'
+        return 'stop'
     elif state[1] == 1:
-        last_state = 'forward'
+        return 'forward'
     elif state[0] == 1:
-        last_state = 'right'
+        return 'right'
     elif state[2] == 1:
-        last_state = 'left'
+        return 'left'
     else:
         print("Charlie is in the bad place, State was: ", state)
-        last_state = 'stop'
-
-    return last_state
+        return 'stop'
+    return 'stop' 
 
 def get_power_level():
     """Prompts the user for a power level between 1 and 100."""
@@ -59,22 +47,17 @@ def main():
     distance = 0
     alpha = 0.012921758
     wheelsize = 0.0205  # Assuming this is the wheel diameter in meters
-
-    start_time = time()  # Record start time
+    run==True
 
     try:
-        while True:
-            current_time = time()
-            elapsed_time = current_time - start_time
+        while run==True:
+            gm_state = get_status()
             distance = (alpha * px_power/1000 + distance)  # Increment distance based on power
             
             if distance / wheelsize >= 2:
                 print("We have crossed the desert to the holy land, 2 meters away.")
-                break
-            
-            gm_state = get_status()
-
-            if gm_state == 'stop':
+                run=False
+            elif gm_state == 'stop':
                 px.forward(0)
                 px.stop()
                 print("Just doing what I was told.")
@@ -87,10 +70,12 @@ def main():
             elif gm_state == 'right':
                 px.forward(px_power)
                 px.set_dir_servo_angle(-offset)
+            else:
+                print("Frank let's play nightcrawlers")
+
             sleep(0.01)  # Sleep at the end of the loop to ensure some delay
 
     finally:
-        px.forward(0)
         px.stop()
         print("Robot stopped.")
 
