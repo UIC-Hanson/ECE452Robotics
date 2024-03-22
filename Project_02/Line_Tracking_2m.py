83af2e61cde2cd5e51824e1560350ef3913d00e9
UIC-Hanson <rhans7@uic.edu>
2024-03-21 20:19:10 -0500

    It's px now. Added sleep .01 made px_power global.

--- Project_02/Line_Tracking_2m.py
+++ Project_02/Line_Tracking_2m.py
@@ -1,13 +1,11 @@
 from picarx import Picarx
 from time import sleep
-#our scripts
-
 from robot_control import get_power_level
+px = Picarx()
 
-picarx = Picarx()
 
 # Define initial variables
-picarx.set_grayscale_reference([1400, 1400, 1400]) #do we need this?
+px.set_grayscale_reference([1400, 1400, 1400]) #do we need this?
 px_power = 0
 alpha = 0.012921758 # Alpha value
 
@@ -15,50 +13,55 @@ last_state = None
 offset = 30
 
 # Initialize servos
-picarx.set_dir_servo_angle(0)
-picarx.set_cam_pan_angle(0)
-picarx.set_cam_tilt_angle(0)
+px.set_dir_servo_angle(0)
+px.set_cam_pan_angle(0)
+px.set_cam_tilt_angle(0)
 
 def handle_out():
     global last_state
     if last_state == 'left' or last_state == 'right':
-        picarx.set_dir_servo_angle(-12 if last_state == 'left' else 12)
-        picarx.backward(5)
-        while picarx.get_line_status(picarx.get_grayscale_data()) == last_state:
+        px.set_dir_servo_angle(-12 if last_state == 'left' else 12)
+        px.backward(5)
+        while px.get_line_status(px.get_grayscale_data()) == last_state:
             sleep(0.001)
 
 def main():
-        try:
-            distance = 0
-            px_power = get_power_level()
-            while True:
-                gm_val_list = picarx.get_grayscale_data()
-                gm_state = picarx.get_line_status(gm_val_list)
-                print("Grayscale Data:%s, Line Status:%s" % (gm_val_list, gm_state))
+    global px_power
+    px_power = get_power_level()
+    print(f"Power level set to: {px_power}")
+    distance = 0
+    
+    try:
+        px.forward(px_power)  # Start moving forward
+        while True:
+            gm_val_list = px.get_grayscale_data()
+            gm_state = px.get_line_status(gm_val_list)
+            print("Grayscale Data:%s, Line Status:%s" % (gm_val_list, gm_state))
 
-                if gm_state != "stop":
-                    last_state = gm_state
+            if gm_state != "stop":
+                last_state = gm_state
 
-                if distance / 0.0205 < 2:
-                    distance += alpha * px_power
+            if distance / 0.0205 < 2:
+                distance += alpha * px_power
+                sleep(0.01)
+            if distance > 0.04:
+                px.stop()
+            elif gm_state == 'forward':
+                px.set_dir_servo_angle(0)
+                px.forward(px_power)
+                sleep(0.01)
+            elif gm_state in ['left', 'right']:
+                px.set_dir_servo_angle(offset if gm_state == 'left' else -offset)
+                px.forward(px_power)
+                sleep(0.01)
+            else:
+                handle_out()
 
-                if distance > 0.04:
-                    picarx.stop()
-
-                elif gm_state == 'forward':
-                    picarx.set_dir_servo_angle(0)
-                    picarx.forward(px_power)
-                elif gm_state in ['left', 'right']:
-                    picarx.set_dir_servo_angle(offset if gm_state == 'left' else -offset)
-                    picarx.forward(px_power)
-                else:
-                    handle_out()
-
-                print('Distance: ' + str(distance / 0.0205))
-                sleep(0.1)
-        finally:
-            picarx.stop()
-            picarx.stop()
+            print('Distance: ' + str(distance / 0.0205))
+            sleep(0.1)
+    finally:
+        px.stop()
+        px.stop()
 
 if __name__ == '__main__':
     main()
