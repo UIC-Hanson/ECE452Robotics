302a048f8b1c33a161c33d453dd20a3ba71b3678
UIC-Hanson <rhans7@uic.edu>
2024-03-21 20:48:35 -0500

    Why

--- Project_02/Line_Tracking_2m.py
+++ Project_02/Line_Tracking_2m.py
@@ -1,6 +1,7 @@
 from picarx import Picarx
 from time import sleep
 from robot_control import get_power_level, get_status
+
 px = Picarx()
 
 def initialize_robot():
@@ -10,12 +11,12 @@ def initialize_robot():
     px.set_cam_pan_angle(0)
     px.set_cam_tilt_angle(0)
 
-def handle_out(last_state):
+def handle_out(px, last_state):
     # Correct the direction based on the last state, reverse!
     correction_angle = -12 if last_state == 'left' else 12
     px.set_dir_servo_angle(correction_angle)
     px.backward(5)
-    while px.get_line_status(px.get_grayscale_data()) == last_state:
+    while get_status() == last_state:
         sleep(0.001)
 
 def main():
@@ -23,7 +24,7 @@ def main():
     px_power = get_power_level()
     print(f"Power level set to: {px_power}")
     distance = 0
-    alpha = 0.012921758 # Alpha value for distance calculation
+    alpha = 0.012921758  # Alpha value for distance calculation
     offset = 30
     last_state = None
 
@@ -34,21 +35,21 @@ def main():
 
             if gm_state != "stop":
                 last_state = gm_state
-                sleep(0.01)
+
             if distance / 0.0205 < 2:
                 distance += alpha * px_power
-                sleep(0.01)
+            
             if distance > 0.04:
                 px.stop()
                 break  # Stop the loop if the distance condition is met
             elif gm_state == 'forward':
                 px.set_dir_servo_angle(0)
-                sleep(0.01)
             elif gm_state in ['left', 'right']:
                 px.set_dir_servo_angle(offset if gm_state == 'left' else -offset)
-                sleep(0.01)
             else:
                 handle_out(px, last_state)
+
+            px.forward(px_power)  # Ensure continuous movement
             print(f'Distance: {distance / 0.0205}')
             sleep(0.1)
     finally:
