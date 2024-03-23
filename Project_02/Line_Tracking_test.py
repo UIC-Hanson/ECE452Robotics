c6ea886cd52ca8708709f527b22aa9632375b49d
UIC-Hanson <rhans7@uic.edu>
2024-03-23 13:23:46 -0500

    combine for 2m line tracking test.

--- Project_02/Line_Tracking_test.py
+++ Project_02/Line_Tracking_test.py
@@ -1,36 +1,55 @@
 from picarx import Picarx
 from time import sleep
 
+# Global Variables
 px = Picarx()
-px = Picarx(grayscale_pins=['1400', '1400', '1400'])
-
-# Please run ./calibration/grayscale_calibration.py to Auto calibrate grayscale values
-# or manual modify reference value by follow code
-# px.set_line_reference([1400, 1400, 1400])
-
 current_state = None
-px_power = 10
-offset = 20
+px_power = 0  # Default power level, can be adjusted via get_power_level()
+offset = 10 #default turning max is 30
 last_state = "stop"
+distance = 0
+alpha = 0.017881989
+wheelsize = 0.0205  # wheel diameter in meters
 
-def outHandle():
-    global last_state, current_state
-    if last_state == 'left':
-        px.set_dir_servo_angle(-30)
-        px.backward(10)
-    elif last_state == 'right':
-        px.set_dir_servo_angle(30)
-        px.backward(10)
+def initialize_robot():
+    """Initialize settings for the robot."""
+    px.set_dir_servo_angle(0)
+    px.set_cam_pan_angle(0)
+    px.set_cam_tilt_angle(0)
+
+def get_power_level():
+    """Prompts the user for a power level between 1 and 100, used in 2mdrive.py."""
     while True:
-        gm_val_list = px.get_grayscale_data()
-        gm_state = get_status(gm_val_list)
-        print("outHandle gm_val_list: %s, %s"%(gm_val_list, gm_state))
-        currentSta = gm_state
-        if currentSta != last_state:
-            break
-    sleep(0.001)
+        try:
+            level = int(input("Enter power level (1-100): "))
+            if 1 <= level <= 100:
+                global px_power
+                px_power = level
+                print(f"Power level set to: {level}")
+                return
+            else:
+                print("Please enter a value between 1 and 100.")
+        except ValueError:
+            print("Invalid input. Please enter a numerical value between 1 and 100.")
+
+def turn():
+    """Turn the robot based on the current state."""
+    inboard_wheel = px_power * 0.95
+    outboard_wheel = px_power
+
+    if current_state == 'left':
+        px.set_dir_servo_angle(-offset)
+        px.set_motor_speed(1, inboard_wheel)
+        px.set_motor_speed(2, outboard_wheel)
+        print("Turning left. Outboard wheel, inboard wheel: %s, %s" % (outboard_wheel, inboard_wheel))
+    elif current_state == 'right':
+        px.set_dir_servo_angle(offset)
+        px.set_motor_speed(1, outboard_wheel)
+        px.set_motor_speed(2, inboard_wheel)
+        print("Turning right. Outboard wheel, inboard wheel: %s, %s" % (outboard_wheel, inboard_wheel))
 
 def get_status(val_list):
+    """Get the current status of the robot based on grayscale sensor input."""
     _state = px.get_line_status(val_list)  # [bool, bool, bool], 0 means line, 1 means background
     if _state == [0, 0, 0]:
         return 'stop'
@@ -41,28 +60,35 @@ def get_status(val_list):
     elif _state[2] == 1:
         return 'left'
 
-if __name__=='__main__':
+if __name__ == '__main__':
+    initialize_robot()
+    get_power_level()  # Get the power level from user input
+
     try:
         while True:
             gm_val_list = px.get_grayscale_data()
-            gm_state = get_status(gm_val_list)
-            print("gm_val_list: %s, %s"%(gm_val_list, gm_state))
+            current_state = get_status(gm_val_list)
+            print("Grayscale values, current state: %s, %s" % (gm_val_list, current_state))
 
-            if gm_state != "stop":
-                last_state = gm_state
+             # Increment distance based on power
+            distance += alpha * (px_power / 2200)
+            if distance / wheelsize >= 2:
+                print("Travelled 2 meters, stopping.")
+                break
 
-            if gm_state == 'forward':
+            if current_state != "stop":
+                last_state = current_state
+
+            if current_state == 'forward':
                 px.set_dir_servo_angle(0)
                 px.forward(px_power)
-            elif gm_state == 'left':
-                px.set_dir_servo_angle(offset)
-                px.forward(px_power)
-            elif gm_state == 'right':
-                px.set_dir_servo_angle(-offset)
-                px.forward(px_power)
+               
+                sleep(0.0001)
+            elif current_state in ['left', 'right']:
+                turn()
+                sleep(0.0001)
             else:
-                outHandle()
+                sleep(0.0001)
     finally:
         px.stop()
-        print("stop and exit")
-        sleep(0.1)
+        print("Robot stopped.")
