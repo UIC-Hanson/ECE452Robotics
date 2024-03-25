0f9c92652a42ca0c7d07cc4288ec141dedd6087e
UIC-Hanson <rhans7@uic.edu>
2024-03-25 13:43:16 -0500

    Move sleep from avoidance maneuver to else loop

--- Project_02/avoid_obstacle2.py
+++ Project_02/avoid_obstacle2.py
@@ -18,24 +18,25 @@ def initialize_robot():
 def avoidance_maneuver():
     """Executes the avoidance maneuver and logs the duration."""
     start_time = time.time()
-    px.set_dir_servo_angle(30)  # Turn right to avoid the obstacle
     px.forward(TURN_SPEED)
+    px.set_dir_servo_angle(30)  # Turn right to avoid the obstacle
     while round(px.ultrasonic.read(), 2) < SAFE_DISTANCE:
         time.sleep(0.01)
     duration = time.time() - start_time
     px.set_dir_servo_angle(0)  # Reset to straight ahead after avoiding the obstacle
-    time.sleep(AVOIDANCE_TIME)  # Wait for 1 second after avoiding the obstacle
+
     return duration
 
 def reciprocal_maneuver(duration):
     """Executes the reciprocal maneuver based on the duration of the initial avoidance."""
-    px.set_dir_servo_angle(-30)  # Turn left to initiate reciprocal maneuver
+      # Turn left to initiate reciprocal maneuver
     time.sleep(duration)  # Match the duration of the initial avoidance
     px.set_dir_servo_angle(0)  # Straighten up
-    px.forward(FORWARD_SPEED)
+    px.forward(TURN_SPEED)
     time.sleep(3)  # Continue straight for 3 seconds
     px.set_dir_servo_angle(-30)  # Turn right to get back on the original course
     time.sleep(duration)  # Match the duration to complete the reciprocal course
+    px.forward(FORWARD_SPEED)
 
 def main():
     try:
@@ -50,7 +51,10 @@ def main():
             else:
                 # Obstacle detected, execute avoidance maneuver
                 duration = avoidance_maneuver()
-                time.sleep(0.01)
+                
+                px.set_dir_servo_angle(-30)
+                time.sleep(AVOIDANCE_TIME)  # Wait after avoiding the obstacle
+
                 # After avoiding, execute reciprocal maneuver to attempt to return to original path
                 reciprocal_maneuver(duration)
                 break  # End loop after executing maneuvers; adjust as necessary for continuous operation
