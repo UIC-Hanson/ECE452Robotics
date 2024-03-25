8a4aa0b83a7563dccad11293b7a04f9e82a2fb2b
Moises <146483403+mloera3@users.noreply.github.com>
2024-03-25 14:00:11 -0500

    Update avoid_obstacle.py

--- Project_02/avoid_obstacle.py
+++ Project_02/avoid_obstacle.py
@@ -83,13 +83,12 @@ def main():
                     V_list.append(px.get_velocity())
                     Θ_list.append(px.get_angle())
 
-                    move_forward(force)
+                    # Implement rectilinear motion with a trajectory angle of 30 degrees
+                    px.set_dir_servo_angle(30)
+                    px.set_velocity(px.get_velocity_threshold())
                 else:
                     stop_movement()
                     
-                # Implement rectilinear motion and record y_threshold and Θ_threshold
-                
-                
                 # After avoiding the obstacle and waiting for ∆t, mirror the previous movements
                 mirror_motion(V_list, Θ_list)
 
