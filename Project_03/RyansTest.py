e4b658219761f906b500de6879d98d7dea17acc3
UIC-Hanson <rhans7@uic.edu>
2024-04-05 15:56:46 -0500

    Test

--- Project_03/RyansTest.py
+++ Project_03/RyansTest.py
@@ -1,9 +1,18 @@
-import cv2 as cv 
+from picamera.array import PiRGBArray
+from picamera import PiCamera
+import time
+import cv2
 
-def testDevice(source):
-   cap = cv.VideoCapture(source) 
-   if cap is None or not cap.isOpened():
-       print('Warning: unable to open video source: ', source)
+#Capture image as array
+camera = PiCamera()
+rawImage = PiRGBArray(camera)
+time.sleep(0.1)
+camera.capture(rawImage, format = "rgb")
+image = rawImage.array
 
-testDevice(0) # no printout
-testDevice(1) # prints message
+# Display captured image in an OpenCV window
+cv2.imshow("Image", image)
+cv2.waitKey(0)
+
+# Record captured image on MicroSD card
+cv2.imwrite("test.jpeg", image)
