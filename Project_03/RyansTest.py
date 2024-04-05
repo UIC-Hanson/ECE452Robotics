cae5f5a73a25d4921e61401a8491d5b34fc1b218
UIC-Hanson <rhans7@uic.edu>
2024-04-05 15:34:42 -0500

    Testing

--- Project_03/RyansTest.py
+++ Project_03/RyansTest.py
@@ -1,26 +1,9 @@
-# import the opencv library 
-import cv2 
-  
-  
-# define a video capture object 
-vid = cv2.VideoCapture(0) 
-  
-while(True): 
-      
-    # Capture the video frame 
-    # by frame 
-    ret, frame = vid.read() 
-  
-    # Display the resulting frame 
-    cv2.imshow('frame', frame) 
-      
-    # the 'q' button is set as the 
-    # quitting button you may use any 
-    # desired button of your choice 
-    if cv2.waitKey(1) & 0xFF == ord('q'): 
-        break
-  
-# After the loop release the cap object 
-vid.release() 
-# Destroy all the windows 
-cv2.destroyAllWindows() 
+import cv2 as cv 
+
+def testDevice(source):
+   cap = cv.VideoCapture(source) 
+   if cap is None or not cap.isOpened():
+       print('Warning: unable to open video source: ', source)
+
+testDevice(0) # no printout
+testDevice(1) # prints message
