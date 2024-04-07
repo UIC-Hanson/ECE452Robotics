4f7a7e6c408a13f0de33317362865b7a3cb03655
UIC-Hanson <rhans7@uic.edu>
2024-04-06 19:53:08 -0500

    dasd
    Rename recordMo testing picamera

--- Project_03/RyansTest.py
+++ Project_03/RyansTest.py
@@ -1,10 +1,12 @@
-import cv2
+from picamera2 import MappedArray, Picamera2, Preview
   
-vid = cv2.VideoCapture(0)
-while(True):
-    ret, frame = vid.read()
-    cv2.imshow('frame', frame)
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
-vid.release()
-cv2.destroyAllWindows()
+vid = Picamera2.VideoCapture(0)
+
+def main():
+    picam2 = Picamera2()
+    picam2.start_preview(Preview.DRM)
+ 
+    picam2.start()
+
+    while True:
+        a = 1;
