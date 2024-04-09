9cc9e58bc3129e3efadc85ed2abc2a737ce85f39
Moises <146483403+mloera3@users.noreply.github.com>
2024-04-09 18:10:04 -0500

    Update Task1a.py

--- Project_03/Task1a.py
+++ Project_03/Task1a.py
@@ -62,7 +62,7 @@ while cap.isOpened():
         corners, ids, rejectedImgPoints = detector.detectMarkers(gray)
 
         if len(corners) != 0:  # if aruco marker detected
-            rvec, tvec, _ = cv2.aruco.estimatePoseSingleMarkers(corners, marker_length, mtx, dist)
+            rvec, tvec, _ = cv2.estimatePoseSingleMarkers(corners, marker_length, mtx, dist)
             cv2.aruco.drawDetectedMarkers(frame, corners, ids, (0,255,0))
             cv2.aruco.drawAxis(frame, mtx, dist, rvec, tvec, 0.05)
         cv2.imshow("aruco", frame)
