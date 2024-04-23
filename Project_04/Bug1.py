a4f1e9ef76180591a6a79446b281ae47faa4574f
UIC-Hanson <rhans7@uic.edu>
2024-04-23 13:49:03 -0500

    From robot

--- Project_04/Bug1.py
+++ Project_04/Bug1.py
@@ -11,26 +11,25 @@ import numpy as np
 import yaml
 import utils
 import math
-from AlphaBot import AlphaBot
 import time
 import pickle
-from TRSensor import TRSensor
+#from TRSensor import TRSensor
 from picarx import Picarx
 
 px = Picarx()
 
 #CHANGE IT FOR AN OPTIMAL RESULT
-px.set_grayscale_reference(0) 
+px.set_grayscale_reference([0,0,0]) 
 
 class point(object):
     def __init__(self):
         point.x = None
         point.z = None
-        
+
 def stop(px):
     px.set_motor_speed(1,0)
     px.set_motor_speed(2,0)
-    
+
 #======= TO DO =======: define 5 methods as for the precedent project
 # turn, move left, move right, move forward, move backward
 
@@ -40,7 +39,7 @@ def turn_left(px):
     time.sleep(0.1)
     px.set_dir_servo_angle(0)
     px.forward(0)
-    
+
 def turn_right(px):
     px.set_dir_servo_angle(90)
     px.forward(10)
@@ -60,7 +59,7 @@ def move_backward(px):
     time.sleep(0.1);
     px.backward(0);
 
-    
+
 '''
 Returns an estimated position of the robot with respect to a line
 The estimate is made using a weighted average of the sensor indices
@@ -89,88 +88,88 @@ def readLine(white_line = 0):
     on_line = 0
     last_value=0
     numSensors=3
-    
+
     #======= TO DO ======= 
     #depending of your calibration you may want to modify these values
     coef1=1000
     treshold1=200
     treshold2=50
-    
+
 
     for i in range(0,numSensors):
-        
-    value = sensor_values[i]
-        
-    if(white_line):
-        value = coef1-value
+
+        value = sensor_values[i]
+
+        if(white_line):
+            value = coef1-value
         # keep track of whether we see the line at all
-    if(value > treshold1):
-        on_line = 1
+        if(value > treshold1):
+            on_line = 1
 
         # only average in values that are above a noise threshold
-    if(value > treshold2):
-        avg += value * (i * coef1);  # this is for the weighted total,
-        sum += value;                  #this is for the denominator
+        if(value > treshold2):
+            avg += value * (i * coef1);  # this is for the weighted total,
+            sum += value;                  #this is for the denominator
 
     if(on_line != 1):
-    # If it last read to the left of center, return 0.
-    if(last_value < (numSensors - 1)*coef1/2):
-    #print("left")
-        return 0;
+        # If it last read to the left of center, return 0.
+        if(last_value < (numSensors - 1)*coef1/2):
+            #print("left")
+            return 0;
 
-    # If it last read to the right of center, return the max.
+        # If it last read to the right of center, return the max.
     else:
-    #print("right")
-        return (numSensors - 1)*coef1
-    
+        #print("right")
+            return (numSensors - 1)*coef1
+
     last_value = avg/sum
     return last_value
-    
-    
-    
-    
-    
+
+
+
+
+
 def go_to_goal(px,cap,goal_id,goal,hit,deg_eps,dist_eps,last_proportional,angle_to_goal):
     print("Warming up the Line track sensors...")
     time.sleep(0.5)
-    
+
     #======= TO DO ======= 
     #depending of your calibration you may want to modify these values (you would also have to modify it in: find_leave
     #and go_to_leave methods)
-    
+
     coef=2000
     treshold=500
-    
-    
+
+
     for i in range(0,100):
         position = readLine()
-        proportional = position - coef1
+        proportional = position - coef
         last_proportional = proportional
     time.sleep(0.5)
     print("Fininished warmup")
     while cap.isOpened():
         ret, frame = cap.read()
         position = readLine()
-        proportional = position - coef1
+        proportional = position - coef
         derivative = proportional - last_proportional
         last_proportional = proportional
         # When the IR sensor detects the line...
-        if abs(derivative) >= treshold1:
+        if abs(derivative) >= treshold:
             state = 1
             print("Hit line...")
             hit.x = p_gc[0]
             hit.z = p_gc[2]
-            
+
             #======= TO DO =======: the robots needs to turn, therefore call here the function implemented before
 
             # Assume that we always go left
             turn_left(px)
-            
-            
+
+
             return state, goal, hit, last_proportional, angle_to_goal
-        
-        
-        
+
+
+
         elif ret:
             gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
             corners, ids, rejectedImgPoints = cv2.aruco.detectMarkers(gray, aruco_dict, parameters=aruco_params)
@@ -215,7 +214,7 @@ def go_to_goal(px,cap,goal_id,goal,hit,deg_eps,dist_eps,last_proportional,angle_
                         turn_right(px)
                 else:
                     turn_left(px) # Look around for the goal      
-                    
+
             cv2.imshow('aruco',frame)
             key = cv2.waitKey(100) & 0xFF
             # Press q to stop in the middle of the process
@@ -224,34 +223,34 @@ def go_to_goal(px,cap,goal_id,goal,hit,deg_eps,dist_eps,last_proportional,angle_
                 stop(px)
                 state = 3
                 return state, goal, hit, last_proportional, angle_to_goal
-                
+
 def find_leave(px,cap,goal_id,helper1_id,helper2_id,goal,hit,leave,dist_eps,g_gh1,g_gh2,last_proportional):
     maximum = 35
     count = 0
     while cap.isOpened():
-        
+
         #======= TO DO =======: call here the function to move the robot backward
         #
         #======================
         move_backward(px)
-        
+
         #======= TO DO ======= 
         #depending of your calibration you may want to modify these values (you would also have to modify it in, find_leave
         #and go_to_leave methods)
-        
+
         coef=2000
-       
-        
+
+
         position = readLine()
         # The "proportional" term should be 0 when we are on the line.
         proportional = position - 2000
-        
+
         # Compute the derivative (change) and integral (sum) of the position.
         derivative = proportional - last_proportional
-        
+
         # Remember the last position.
         last_proportional = proportional
-        
+
         #======= TO DO =======
         power_difference = proportional/25 + derivative/100 #+integral/500
         if (power_difference > maximum):
@@ -259,22 +258,22 @@ def find_leave(px,cap,goal_id,helper1_id,helper2_id,goal,hit,leave,dist_eps,g_gh
         if (power_difference < - maximum):
             power_difference = - maximum
         if (power_difference < 0):
-            
+
             #!!!!!!!!!!!!!!!!!CARE!!!!!!!!!!!!!!!!!! 
             #DEPENDING OF YOUR CALIBRATION YOU MAY HAVE TO INVERSE 1 AND 2 HERE BELOW
             #Also you may want to add a steering angle deping on how the robot is able to slip
             px.set_motor_speed(1,maximum + power_difference)
             px.set_motor_speed(2,maximum)
-            
+
         else:
-            
+
             #!!!!!!!!!!!!!!!!!CARE!!!!!!!!!!!!!!!!!! 
             #DEPENDING OF YOUR CALIBRATION YOU MAY HAVE TO INVERSE 1 AND 2 HERE BELOW
             #Also you may want to add a steering angle deping on how the robot is able to slip
             px.set_motor_speed(1,maximum)
             px.set_motor_speed(2,maximum - power_difference)
-            
-            
+
+
         time.sleep(0.05)
         stop(px)
         ret, frame = cap.read()
@@ -298,15 +297,15 @@ def find_leave(px,cap,goal_id,helper1_id,helper2_id,goal,hit,leave,dist_eps,g_gh
                             leave.z = p_gc[2]
                             count += 1
                         if cur_dist_hit <= dist_eps and count > 100:
-                            
-                            
+
+
                             stop(px)
                             print("Finished the track, now going to the leave point")
                             state = 2
                             return state, leave, last_proportional
-                        
-                        
-                        
+
+
+
                     # ======= TO DO =======:: Please fill out when other markers are detected.
                     #       The code is very similar to above where the Goal marker is detected.
                     elif ids[i] == helper1_id:
@@ -318,66 +317,66 @@ def find_leave(px,cap,goal_id,helper1_id,helper2_id,goal,hit,leave,dist_eps,g_gh
             key = cv2.waitKey(100) & 0xFF
             if key == ord('q'):
                 cv2.destroyAllWindows()
-                
-                
+
+
                 stop(px)
                 state = 3
                 return state, leave, last_proportional
-            
-            
-            
-            
-            
-            
+
+
+
+
+
+
 
 def go_to_leave(px,cap,goal_id,helper1_id,helper2_id,goal,leave,dist_eps,g_gh1,g_gh2,last_proportional):
     maximum = 35
     while cap.isOpened():
-        
-        
+
+
         #======= TO DO =======: call here below your backward method
         move_backward(px)
         #======================
-        
-        
+
+
         #======= TO DO ======= 
         #depending of your calibration you may want to modify these values (you would also have to modify it in, find_leave
         #and go_to_leave methods)
         coef=2000
-        
-        
-        
+
+
+
         position = TR.readLine()
-        
+
         # The "proportional" term should be 0 when we are on the line.
         proportional = position - coef
-        
+
         # Compute the derivative (change) and integral (sum) of the position.
         derivative = proportional - last_proportional
-        
+
         # Remember the last position.
         last_proportional = proportional
         power_difference = proportional/25 + derivative/100 #+integral/500
-        
-        
+
+
         if (power_difference > maximum):
             power_difference = maximum
         if (power_difference < - maximum):
             power_difference = - maximum
         if (power_difference < 0):
-            
+
             #!!!!!!!!!!!!!!!!!CARE!!!!!!!!!!!!!!!!!! 
             #DEPENDING OF YOUR CALIBRATION YOU MAY HAVE TO INVERSE 1 AND 2 HERE BELOW
             #Also you may want to add a steering angle deping on how the robot is able to slip
             px.set_motor_speed(1,maximum + power_difference)
             px.set_motor_speed(2,maximum)
-            
+
         else:
-            
+
             #SAME THING HERE
             px.set_motor_speed(1,maximum)
             px.set_motor_speed(2,maximum - power_difference)
-            
+
         time.sleep(0.05)
         stop(px)
         ret, frame = cap.read()
@@ -397,11 +396,11 @@ def go_to_leave(px,cap,goal_id,helper1_id,helper2_id,goal,leave,dist_eps,g_gh1,g
                             for i in range(5):
                                 #======= TO DO =======: call your forward function here below
                                 move_forward(px)
-                                
+
                             state = 0
                             return state
-                        
-                        
+
+
                     # ======= TO DO =======:: Please fill out when other markers are detected.
                     #       The code is very similar to above where the Goal marker is detected.
                     elif ids[i] == helper1_id:
@@ -422,9 +421,9 @@ def go_to_leave(px,cap,goal_id,helper1_id,helper2_id,goal,leave,dist_eps,g_gh1,g
 
 last_proportional = 0
 
-with open('ir_calib.pkl', 'rb') as ir_calib_file:
-    TR = pickle.load(ir_calib_file)
-    stop(px)
+#with open('ir_calib.pkl', 'rb') as ir_calib_file:
+#    TR = pickle.load(ir_calib_file)
+#    stop(px)
 
 # The different ArUco dictionaries built into the OpenCV library. 
 aruco_dict = cv2.aruco.Dictionary_get(cv2.aruco.DICT_5X5_250)
@@ -451,8 +450,8 @@ with open(r'calib_data_bug1.yaml') as file:
 
 mtx = np.asarray(calib_data["camera_matrix"])
 dist = np.asarray(calib_data["distortion_coefficients"])
-g_gh1 = np.asarray(calib_data["g_gh1"])
-g_gh2 = np.asarray(calib_data["g_gh2"])
+#g_gh1 = np.asarray(calib_data["g_gh1"])
+#g_gh2 = np.asarray(calib_data["g_gh2"])
 
 cap = cv2.VideoCapture(cv2.CAP_V4L)
 
