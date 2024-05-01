5895da2ef5e4f9afedd6fb0c965ad2d2a4a28ca9
Moises <mloera3@uic.edu>
2024-05-01 16:14:20 -0500

    Update Bug_1_main.py

--- Project_04/Bug_1_main.py
+++ Project_04/Bug_1_main.py
@@ -20,7 +20,7 @@ def DetermineGoal():
 # Returns 'left', 'right', or 'forward'
 def read_line(px):
     sensor_values = px.get_grayscale_data()
-    threshold = 75 # Arbitrary, set this later
+    threshold = 100 # Arbitrary, set this later
 
     detected = []
     for i in range(3):
